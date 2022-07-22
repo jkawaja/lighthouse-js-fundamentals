3a3bbf7da4b1b763ffe67a2b28077fbd314ee170
Justin Kawaja <jkawaja909@gmail.com>
2022-07-22 15:33:12 -0230

    Udacity Exercise for Factorials; received feedback from Techie to turn into moddable file.

---
+++ Factorials.js
@@ -0,0 +1,16 @@
+let solution = 12;
+
+for (let x = 11; x >= 1; x--) {
+  solution *= x;
+}
+
+console.log(solution);
+
+// Techie suggestion: a factorial function! Just change solution's value
+let factorial = ;
+
+for (let x = factorial - 1; x >= 1; x--) {
+  factorial *= x;
+}
+
+console.log(factorial);
