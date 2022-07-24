2a6ea0a65fc2c17b164625f26f309ab5cd285a18
Justin Kawaja <jkawaja909@gmail.com>
2022-07-24 18:57:21 -0230

    Challenge to create function to determine if a number is odd or not

---
+++ odd.js
@@ -0,0 +1,8 @@
+const isOdd = function (num) {
+  return num % 2 != 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("4 is odd: " + isOdd(4));
+console.log("9 is odd: " + isOdd(9));
